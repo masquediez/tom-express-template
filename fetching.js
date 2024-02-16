d4f18a10ffb23343bc5a075b73605b1755eeafc9
gabriel <gabrielmasquediez@gmail.com>
2024-02-16 22:20:14 +0100

    fetching.js

--- fetching.js
+++ fetching.js
@@ -1,3 +1,11 @@
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+fetch("http://localhost:5050/v1/todos/all")
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+
+fetch("http://localhost:5050/v1/todos/byuserid?userId=2")
+  .then((res) => res.json())
+  .then((json) => console.log(json));
